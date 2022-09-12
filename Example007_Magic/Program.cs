bf084feb999c79783603be056d508d41e0e75278
Anna <annamosolowa@gmail.com>
2022-09-12 23:00:13 +1000

    Точки

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -1,6 +1,9 @@
 ﻿Console. Clear ();
-int xa = 1, ya = 1, 
-    xb = 1, yb = 30,
+//Console. SetCursorPosition (10,4);
+//Console.WriteLine ("+");
+
+int xa = 30, ya = 3, 
+    xb = 3, yb = 15,
     xc = 60, yc = 30;
 Console.SetCursorPosition (xa, ya);
 Console.WriteLine ("+");
@@ -10,3 +13,31 @@ Console.WriteLine ("+");
 
 Console. SetCursorPosition (xc, yc);
 Console.WriteLine ("+");
+
+
+int x = xa, y = xb;
+
+int count = 0;
+
+while (count < 1000)
+{
+    int what = new Random().Next (0,3); // [0;3] 0 1 2
+    if (what == 0)
+    {
+        x = (x+xa)/2;
+        y = (y+ya)/2;
+    }
+    if (what == 1)
+    {
+        x = (x+xb)/2;
+        y = (y+yb)/2;
+    }
+    if (what == 2)
+    {
+        x = (x+xc)/2;
+        y = (y+yc)/2;
+    }
+    Console.SetCursorPosition(x,y);
+    Console.WriteLine ("+");
+    count = count + 1;
+}
